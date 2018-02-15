e3ee972eb60f476c370dba13a677ab310f22f454
ryannewman2828 <ryannewman2828@gmail.com>
2018-02-14 21:44:07 -0800

    finished main functionality

--- app/controllers/skills_controller.py
+++ app/controllers/skills_controller.py
@@ -1,17 +1,26 @@
 from app.models.skills import Skill
-from flask import jsonify
+from flask import jsonify, request
 from sqlalchemy.sql import func
-from sqlalchemy import distinct
 from flask_restful import Resource
 from app.models.database import db_session
 
 
 class SkillController(Resource):
     def get(self):
+        frequency = request.args.get('frequency')
+        rating = request.args.get('rating')
+        if rating is None:
+            rating = 0
+        if frequency is None:
+            frequency = 0
+        print frequency
         skills_stats = db_session.query(Skill.name,
                                         func.avg(Skill.rating).label('average'),
                                         func.count(Skill.user_id).label('count'))\
-            .group_by(Skill.name).all()
+            .group_by(Skill.name)\
+            .having(func.count(Skill.user_id) > int(frequency)) \
+            .having(func.avg(Skill.rating) > int(rating)) \
+            .all()
         skills_stats = map(lambda skill: {'skill': skill[0], 'average': skill[1], 'users': skill[2]},
                            skills_stats)
         return jsonify(skills_stats)
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,20 +1,21 @@
 from app.models.users import User
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Resource
 from app.models.database import db_session
 
 
 class UserController(Resource):
     def get(self, user_id):
-        user = User.query.get(user_id).as_dict()
+        user = User.query.get(user_id)
         if user is not None:
+            user = user.as_dict()
             return jsonify(user)
-        return 'User not Found', 404
+        return {"error": "User not Found"}, 404
 
     def put(self, user_id):
         user = User.query.get(user_id)
         if user is not None:
-            args = self.parser.parse_args()
+            args = request.get_json(silent=True)
             for key, value in args.items():
                 if args[key] is not None:
                     setattr(user, key, args[key])
